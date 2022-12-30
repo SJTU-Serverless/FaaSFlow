236eac075b45ac8bee15a1bb6ac615e83cbfbcfd
lambda7xx <lambda7xx@gmail.com>
2022-12-30 23:19:48 +0800

    fix the workrsp_setup.bash

--- scripts/worker_setup.bash
+++ scripts/worker_setup.bash
@@ -21,5 +21,3 @@ docker build --no-cache -t workflow_base ../src/container
 python3 ../benchmark/generator/translator.py
 ../benchmark/wordcount/create_image.sh
 ../benchmark/fileprocessing/create_image.sh
-../benchmark/illgal_recognizer/create_image.sh
-../benchmark/video/create_image.sh
